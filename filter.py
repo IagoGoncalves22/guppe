8467fee95fd2f1e8279295ec716cf06474b0eaf9
Iago <iago.g.goncalves@gmail.com>
2023-09-25 23:13:14 -0300

    aula filter concluído

--- filter.py
+++ filter.py
@@ -3,7 +3,8 @@ Filter
 
 filter() -> Serve para filtrar dadosd e uma determinada coleção.
 
-"""
+....................
+
 # Biblioteca para trabalhar com dados estatísticos
 import statistics
 
@@ -28,6 +29,77 @@ print(list(res))
 
 print(f'Novamente: {list(res)}') # Na segunda vez zera o valor, igual Map
 
+....................
 
+paises = ['', 'Argentina', '', 'Brasil', 'Chile', '', 'Colombia', '', 'Equador', '', '', 'Venezuela']
+
+print(paises)
+
+res = filter(None, paises)
+
+print(list(res))
+
+....................
+
+paises = ['', 'Argentina', '', 'Brasil', 'Chile', '', 'Colombia', '', 'Equador', '', '', 'Venezuela']
+
+# res = filter(lambda pais: len(pais) > 0, paises)
+# res = filter(None, paises)
+res = filter(lambda pais: pais != '', paises)
+
+print(list(res))
+
+....................
+
+# A diferena entre map() e filter() é:
+
+# map() -> Recebe dois parâmetros, uma função e um iterável e retorna um objeto
+# mapeando a função para cada elemento do iterável.
+
+# Filter() -> Recebe dois parãmetros, uma função e um iterável e retorna um objeto
+# filtrando apenas os elementos de acordo com a função.
+
+....................
+
+# Exemplo mais complexo
+
+usuarios = [
+    {"username": "samuel", "tweets": ["Eu adoro bolos!", "Eu adoro pizza"]},
+    {"username": "Carla", "tweets": ["Eu amo meus gatos"]},
+    {"username": "jeff", "tweets": []},
+    {"username": "bob123", "tweets": []},
+    {"username": "doggo", "tweets": ["Eu gosto de cachoorro", "Eu vou sair hoje"]},
+    {"username": "gal", "tweets": []}
+]
+
+print(usuarios)
+
+# Filtrar os usuários que estão inativos no Twiter
+
+# Forma 1
+# inativos = list(filter(lambda usuario: len(usuario['tweets']) == 0, usuarios))
+
+# forma 2
+inativos = list(filter(lambda usuario: not usuario['tweets'], usuarios))
+
+....................
+
+....................
+
+....................
+
+....................
+
+"""
+# Combinar filter() e map()
+
+nomes = ['Vanessa', 'Ana', 'Maria']
+
+# Devemos criar uma lista contendo 'Sua instrutora é' + nome, desde que
+# cada nome tenha menos de 5 caracteres.
+
+lista = list(map(lambda nome: f'Sua instrutora é {nome}', filter(lambda nome: len(nome) < 5, nomes)))
+
+print(lista)
 
 
