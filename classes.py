aa7b29f701e8a14433f10172a81e0de61a573e91
Iago <iago.g.goncalves@gmail.com>
2023-09-30 17:27:35 -0300

    aula sobre classes concluído

---
+++ classes.py
@@ -0,0 +1,50 @@
+"""
+POO - Classes
+
+Em POO, Classes nada mais são do que modelos dos objetos do mundo real sendo
+representados computacionalmente.
+
+Imagine que você queira fazer um sistema para automatizar o controle das lampadas
+da sua casa.
+
+Classes podem conter:
+    - Atributos -> Representam as características do objeto. Ou seja, pelos atributos
+    conseguimos representar computacionalmente os estados de um objeto. No caso da lampada
+    possivelmente iríamos querer saber se a lâmpada é 110 ou 220 volts, se ela é branca, amarela,
+    vermelha ou outra cor, qual é a luminosidade dela e etc...
+
+    - Métodos (funções) -> Representam os comportamentos do objeto. Ou seja, as ações que esse
+    objeto pode ralizar no seu sistema. No caso da lâmpada, por exemplo, um comportamento comum
+    que muito provavelmente iríamos querer representar no nosso sistema é o de ligar ou desligar.
+
+Em Python, para definir uma classe utilizamos a palavra chave reservada class.
+
+OBS: Utilizamos a palavra 'pass' em Python quando temos um bloco de código que ainda não está implementado.
+
+OBS: Quando nomeamos nossas classe em Python utilizamos por convenção o nome com inicial em maiusculo.
+Se o nome for composto, utiliza-se as iniciais de ambas as palavras em maiusculas, todas juntas.
+
+Dica Geek: Em computação não utilizamos: Acentuação,caracteres especiais, espaços ou similares para nomes
+de classes, atributos, métodos, arquivos, diretórios, etc..
+
+OBS: Quando estamos planejando um software e definimos quais classes temos que ter no sistema, chamamos estes
+objetos que serão mapeados para classes de entidade.
+
+
+"""
+
+class Lampada:
+    pass
+
+class ContaCorrente:
+    pass
+
+class Produto:
+    pass
+
+class Usuario:
+    pass
+
+valor = int('42') # cast
+
+print(help(int))
