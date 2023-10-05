17f0a290109b54b567801a627c218bd1aea3e9f9
Iago <iago.g.goncalves@gmail.com>
2023-10-05 09:36:23 -0300

    aula MyPy, concluído

---
+++ aula05_type_mypy.py
@@ -0,0 +1,17 @@
+def cabecalho(texto: str, alinhamento: bool = True) -> str:
+    if alinhamento:
+        return f"{texto.title()}\n{'-' * len(texto)}"
+    else:
+        return f"{texto.title()} ".center(50, '#')
+
+
+print(cabecalho('Geek University'))
+
+print(cabecalho(' Geek University', alinhamento=False))
+
+
+print(cabecalho(' Geek University ', alinhamento=True)) # vai rodar porque string é True.
+# Podemos fazer a checagem com o MyPy (terminal, mypy nome_do_arquivo.py)
+
+# OBS: utilizar junto o Type Hinting e o MyPy para checar. Dar robustez e segurança ao programa.
+
