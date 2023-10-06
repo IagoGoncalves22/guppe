c1701c8b7da250b778d34831595310f8430bfceb
Iago <iago.g.goncalves@gmail.com>
2023-10-06 08:23:09 -0300

    aula sobre tipos de dados, concluído

--- tipos_de_dados_mais_precisos.py
+++ tipos_de_dados_mais_precisos.py
@@ -32,12 +32,6 @@ def calcula_v1(operacao: str, num1: int, num2: int) -> None:
 
 ......................
 
-......................
-
-......................
-
-"""
-
 # Literal type
 
 from typing import Literal
@@ -58,7 +52,105 @@ calcula_v2('+', 6, 4)
 calcula_v2('-', 10, 2)
 calcula_v2('*', 3, 5)
 
+......................
+
+# Union
+
+from typing import Union
+
+def soma(num1: int, num2: int) -> Union[str, int]:
+    resultado: int = num1 + num2
+
+    if resultado > 50:
+        return f'O valor {resultado} é muito grande.'
+    else:
+        return resultado
+
+
+......................
+
+# Final
+
+from typing import Final
+
+NOME: Final = 'Geek'
+
+print(NOME)
+
+NOME = 'University'
+
+print(NOME)
 
 
 
+......................
 
+from typing import final
+
+@final
+class Pessoa:
+    pass
+
+class Estudante():
+    pass
+
+    @final
+    def estudar(self):
+        print('Estou estudando...')
+
+class Estagiario(Estudante):
+    pass
+
+    def estudar(self):
+        print('Estudando e estagiando...')
+
+
+# O mypy deve acusar que não dá para herdar a classe, pois colocamos o decorador @final
+
+......................
+
+# typed Dictionaries
+
+from typing import TypedDict
+
+class CursoPython(TypedDict):
+    versao: str
+    atualizacao: int
+
+geek = CursoPython(versao='3.8.5', atualizacao=2020)
+
+print(geek)
+
+outro = CursoPython(algo='vai', coisa=True)
+
+print(outro)
+
+......................
+
+......................
+
+......................
+
+"""
+
+# Protocols - Vai modelar o comportamento
+
+from typing import Protocol
+
+class Curso(Protocol):
+    titulo: str
+
+def estudar(valor: Curso) -> None:
+    print(f'Estou estudando o curso {valor.titulo}')
+
+class Venda:
+    titulo = 'Oi'
+
+
+v1 = Venda()
+# c1 = Curso()
+
+
+
+# estudar(c1)
+estudar(v1)
